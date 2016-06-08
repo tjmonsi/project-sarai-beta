f74ffdfbfb29b3cbbb6caf5f77c402e864aab811
Monina Carandang <reactor04@gmail.com>
2016-06-08 14:29:43 +0800

    Minor fixes.

--- client/modules/main/v1/components/header.jsx
+++ client/modules/main/v1/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 class Header extends React.Component {
   componentDidMount() {
@@ -12,9 +13,13 @@ class Header extends React.Component {
     }
   }
   render() {
+    const headerClass = classNames('mdl-cell mdl-cell--10-col-desktop mdl-cell--1-offset-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone')
+
     return (
-      <div className="">
-        Header goes here
+      <div className="mdl-grid">
+        <div className={headerClass}>
+          Header goes here
+        </div>
       </div>
     );
   }
