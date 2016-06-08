0617de45c5d39b872f2c83a150b4d67da3e0ec5c
Monina Carandang <reactor04@gmail.com>
2016-06-08 16:00:23 +0800

    Styled header links.

--- client/modules/main/v1/components/header.jsx
+++ client/modules/main/v1/components/header.jsx
@@ -20,11 +20,13 @@ class Header extends React.Component {
   renderLinks() {
     const {getHeaderLinks} = this.props
     const headerLinks = getHeaderLinks()
-    console.log(headerLinks)
+
+    const linkCell  = classNames('mdl-cell mdl-cell--2-col mdl-cell--4-col-phone')
+
     return headerLinks.map((link, key) => {
       return (
-        <div key={key}>
-          {link.label}
+        <div className={linkCell} key={key}>
+          <a className="main-header-link" href={link.href}>{link.label}</a>
         </div>
       )
     })
@@ -33,20 +35,21 @@ class Header extends React.Component {
   render() {
     const headerClass = classNames('mdl-cell mdl-cell--10-col-desktop mdl-cell--1-offset-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone')
 
-    const {logo} = this.props
-
+    // const {logo} = this.props
+    const logo = '/images/main/project-sarai-logo-green.jpg'
 
     return (
       <div className="mdl-grid">
         <div className={headerClass}>
           <div className="mdl-grid">
-            Header goes here
             <div className="mdl-cell--3-col mdl-cell--4-col-phone">
               <img className="main-header-logo" src={logo} />
             </div>
 
-            <div className="main-header-links">
-              {this.renderLinks()}
+            <div className="mdl-cell--9-col mdl-cell--4-col-phone">
+              <div className="mdl-grid">
+                {this.renderLinks()}
+              </div>
             </div>
 
           </div>
