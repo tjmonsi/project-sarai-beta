f2968491e67a2cd10e7332b78238133761e8fe4a
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-06-15 07:19:48 +0000

    Fix

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 // Import React and JS
 import '/node_modules/material-design-lite/material.js';
+import '/node_modules/material-design-lite/material.css';
+import '/imports/client/js/modules/comp-lib/v2/index.scss';
 // import '/node_modules/mdl-ext/lib/mdl-ext.js';
 import {createApp} from 'mantra-core';
 import {initContext} from './../imports/client/js/configs/context';
